5a2909d1016feac7c375bf43967ab3051420812f
Jesko Appelfeller <jesko@appelfeller.eu>
2017-08-14 15:27:48 +0200

    add preprocessor variable and ifdefs based on it

--- gcc-arm.cmake
+++ gcc-arm.cmake
@@ -4,10 +4,10 @@
 set (CMAKE_SYSTEM_NAME Linux)
 #target processor is an arm with a version number <8
 #this is relevant since the only problematic part of rawrtc is the intel-specific assembler code in crc32c.c and arm supports its own assembler instructions since version 8
-set (CMAKE_SYSTEM_PROCESSOR arm<8)
+set (CMAKE_SYSTEM_PROCESSOR arm)
 
 #set the compilers
 set (CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
 set (CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
 
-#Note: Alternate search paths for libraries, etc. are not set, it is assumed that cross-compilation will take place in a docker container
+#Note: search path for libraries etc. is not changed, we are assuming that compilation is taking place in a docker container which handles those things
