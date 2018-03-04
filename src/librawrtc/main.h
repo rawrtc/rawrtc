6565afa9ea103a4ec8b2507e8dd4897215e3ce92
Lennart Grahl <lennart.grahl@gmail.com>
2018-03-04 06:53:54 +0100

    Add RAWRTCDC timer handler

--- src/librawrtc/main.h
+++ src/librawrtc/main.h
@@ -1,2 +1,12 @@
 #pragma once
 #include <rawrtc.h>
+
+extern struct rawrtc_global rawrtc_global;
+
+/*
+ * Global RAWRTC vars.
+ */
+struct rawrtc_global {
+    struct tmr rawrtcdc_timer;
+    uint_fast16_t rawrtcdc_timer_interval;
+};
